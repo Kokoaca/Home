fbcc5559bf8997abd8c2a7ea4e6ce3dc073b49b9
jhpark <whdgus4120@kyonggi.ac.kr>
2024-10-05 22:07:39 +0900

    branch1 commit

--- rng.py
+++ rng.py
@@ -1,7 +1,8 @@
 num1 = int(input("첫번째 숫자 : "))
 num2 = int(input("두번째 숫자 : "))
+num3 = int(input("세번째 숫자 : "))
 
-print(num1 +num2)
-print(num1 -num2)
-print(num1*num2)
-print(format(num1/num2, ".2f"))
+print(num1 +num2 +num3)
+print(num1 -num2 -num3)
+print(num1*num2*num3)
+print(format(num1/num2/num3, ".4f"))
